a5e9540f80e8b54a293f00a8b1384784d7b08a11
Lans12345 <olanalans12345@gmail.com>
2023-05-03 13:27:45 +0800

    decoded login error

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -105,6 +105,18 @@ class LoginScreen extends StatelessWidget {
           password: passwordController.text);
       Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => const SplashToHomeScreen()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        showToast("No user found with that username.");
+      } else if (e.code == 'wrong-password') {
+        showToast("Wrong password provided for that user.");
+      } else if (e.code == 'invalid-email') {
+        showToast("Invalid username provided.");
+      } else if (e.code == 'user-disabled') {
+        showToast("User account has been disabled.");
+      } else {
+        showToast("An error occurred: ${e.message}");
+      }
     } on Exception catch (e) {
       showToast("An error occurred: $e");
     }
