c01b90b31a2573a6e3e8ab04a96c17df297eb3f3
Lans12345 <olanalans12345@gmail.com>
2023-05-12 13:40:45 +0800

    added validators

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -13,108 +13,116 @@ class LoginScreen extends StatelessWidget {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: grey,
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/back.png'),
-                fit: BoxFit.cover)),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30, 50, 30, 50),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                TextBold(text: 'PHara', fontSize: 58, color: Colors.white),
-                const SizedBox(
-                  height: 75,
-                ),
-                TextRegular(text: 'Login', fontSize: 24, color: Colors.white),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFieldWidget(
-                  textCapitalization: TextCapitalization.none,
-                  hint: 'Username',
-                  label: 'Username',
-                  controller: emailController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a user';
-                    }
-
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFieldWidget(
-                  textCapitalization: TextCapitalization.none,
-                  showEye: true,
-                  isObscure: true,
-                  hint: 'Password',
-                  label: 'Password',
-                  controller: passwordController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a password';
-                    }
-                    if (value.length < 8) {
-                      return 'Password must be at least 8 characters long';
-                    }
-                    final hasUppercase = value.contains(RegExp(r'[A-Z]'));
-                    final hasLowercase = value.contains(RegExp(r'[a-z]'));
-                    final hasNumber = value.contains(RegExp(r'[0-9]'));
-                    if (!hasUppercase || !hasLowercase || !hasNumber) {
-                      return 'Password must contain at least one uppercase letter, one lowercase letter, and one number';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Center(
-                  child: ButtonWidget(
-                    color: black,
-                    label: 'Login',
-                    onPressed: (() {
-                      login(context);
-                    }),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/back.png'),
+                  fit: BoxFit.cover)),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(30, 50, 30, 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 50,
                   ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextRegular(
-                        text: "New to PHara?",
-                        fontSize: 12,
-                        color: Colors.white),
-                    TextButton(
+                  TextBold(text: 'PHara', fontSize: 58, color: Colors.white),
+                  const SizedBox(
+                    height: 75,
+                  ),
+                  TextRegular(text: 'Login', fontSize: 24, color: Colors.white),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFieldWidget(
+                    textCapitalization: TextCapitalization.none,
+                    hint: 'Username',
+                    label: 'Username',
+                    controller: emailController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a user';
+                      }
+
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFieldWidget(
+                    textCapitalization: TextCapitalization.none,
+                    showEye: true,
+                    isObscure: true,
+                    hint: 'Password',
+                    label: 'Password',
+                    controller: passwordController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      if (value.length < 8) {
+                        return 'Password must be at least 8 characters long';
+                      }
+                      final hasUppercase = value.contains(RegExp(r'[A-Z]'));
+                      final hasLowercase = value.contains(RegExp(r'[a-z]'));
+                      final hasNumber = value.contains(RegExp(r'[0-9]'));
+                      if (!hasUppercase || !hasLowercase || !hasNumber) {
+                        return 'Password must contain at least one uppercase letter, one lowercase letter, and one number';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Center(
+                    child: ButtonWidget(
+                      color: black,
+                      label: 'Login',
                       onPressed: (() {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => SignupScreen()));
+                        if (_formKey.currentState!.validate()) {
+                          login(context);
+                        }
                       }),
-                      child: TextBold(
-                          text: "Signup Now",
-                          fontSize: 14,
-                          color: Colors.white),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextRegular(
+                          text: "New to PHara?",
+                          fontSize: 12,
+                          color: Colors.white),
+                      TextButton(
+                        onPressed: (() {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => SignupScreen()));
+                        }),
+                        child: TextBold(
+                            text: "Signup Now",
+                            fontSize: 14,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
